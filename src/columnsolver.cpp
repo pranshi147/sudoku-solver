6c3fcb6c67016ac0a132a73042d078f5bcba7fca
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-07 13:28:22 +0530

    Column solver function created

---
+++ src/columnsolver.cpp
@@ -0,0 +1,9 @@
+#include "sudoku.h"
+#include "columnsolver.h"
+
+bool columnSolver(Sudoku *s, int *column, char *number){
+    for(int i=0; i<9; i++){
+        if(s->box[i][*column]==*number) return false;
+    }
+    return true;
+}
---
+++ src/columnsolver.h
@@ -0,0 +1,9 @@
+#ifndef COLUMNSOLVER_H
+#define COLUMNSOLVER_H
+
+#include "sudoku.h"
+
+class Sudoku; 
+bool columnSolver(Sudoku *s, int *column, char* number);
+
+#endif
