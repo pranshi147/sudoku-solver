f29275edf9bc201b151ccbd509cb8e01bcd5494d
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-09 19:50:52 +0530

    Enhanced sudoku display model

--- src/display.cpp
+++ src/display.cpp
@@ -2,10 +2,13 @@
 #include "display.h"
 
 void display(Sudoku *s){
+    std::cout<<"-------------------"<<std::endl;
     for(int i=0; i<9; i++){
+        std::cout<<"|";
         for(int j=0; j<9; j++){
-            std::cout<<s->box[i][j];
+            std::cout<<s->box[i][j]<<"|";
         }
-        std::cout<<std::endl;
+        std::cout<<std::endl<<"-------------------"<<std::endl;
     }
 }
+
